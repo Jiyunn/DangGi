953740fe5da403c8c291b82fded42fea304a064f
JY <jygr0309@gmail.com>
2018-02-07 10:07:57 +0900

    #16-메모삭제기능추가

--- app/src/main/java/me/jy/danggi/activities/MainActivity.java
+++ app/src/main/java/me/jy/danggi/activities/MainActivity.java
@@ -8,74 +8,59 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import me.jy.danggi.R;
 import me.jy.danggi.activities.adapter.MemoAdapter;
+import me.jy.danggi.activities.model.Memo;
 import me.jy.danggi.database.DataHelper;
 import me.jy.danggi.databinding.ActivityMainBinding;
-import me.jy.danggi.activities.model.Memo;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-
-    private RecyclerView.LayoutManager layoutManager;
     private MemoAdapter adapter;
-    private List<Memo> dataSet;
-
-    DataHelper mDbHelper;
+    private DataHelper mDbHelper;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        dataSet = new ArrayList<>();
-
         initRecyclerView();
+    }
+
+    @Override
+    protected void onStart () {
+        super.onStart();
+        Log.d("jy", "onStart");
         getMemoData();
     }
 
-    @Override
-    protected void onDestroy() {
-        mDbHelper.close();
-        super.onDestroy();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_write:
-                startActivity(new Intent(getApplicationContext(), WriteActivity.class));
-                return true;
-            default:
-                return true;
-        }
-
-    }
-
-    public void initRecyclerView() {
-        adapter = new MemoAdapter(dataSet);
-        layoutManager = new LinearLayoutManager(getApplicationContext());
-
+    public void initRecyclerView () {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        adapter = new MemoAdapter();
+        adapter.getPublishSubject()
+                .subscribe(s -> {
+                    deleteMemoData(adapter.getDataFromPosition(s));
+                    adapter.deleteDataSet(s);
+                });
         binding.recyclerviewMain.setHasFixedSize(true);
         binding.recyclerviewMain.setAdapter(adapter);
         binding.recyclerviewMain.setLayoutManager(layoutManager);
     }
 
-    private void getMemoData() {
+    private void deleteMemoData ( Memo memo ) {
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        String selection = DataHelper.DataEntry.COLUMN_NAME_CONTENT + " LIKE ?";
+        String[] selectionArgs = {memo.getContent()};
+
+        db.delete(DataHelper.DataEntry.TABLE_MEMO, selection, selectionArgs);
+    }
+
+    private void getMemoData () {
         mDbHelper = new DataHelper(this);
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
         String sortOrder =
@@ -83,19 +68,42 @@ public class MainActivity extends AppCompatActivity {
 
         Cursor cursor = db.query(
                 DataHelper.DataEntry.TABLE_MEMO,
-                new String[]{DataHelper.DataEntry.COLUMN_NAME_CONTENT, DataHelper.DataEntry.COLUMN_NAME_WRITE_DATE},
-                null,
-                null,
-                null,
-                null,
-                sortOrder);
+                new String[]{DataHelper.DataEntry.COLUMN_NAME_CONTENT , DataHelper.DataEntry.COLUMN_NAME_WRITE_DATE},
+                null, null, null, null, sortOrder);
 
-        while (cursor.moveToNext()) {
-            dataSet.add(new Memo(cursor.getString(0), cursor.getString(1)));
+        if(adapter.getItemCount() >0) //이미 데이터가 있으면, 있던거 지우고 다시 등록.. 근데 이거되게 비효율적인거같아..!
+            adapter.deleteDataSet();
+
+
+
+        while ( cursor.moveToNext() ) {
+            adapter.updateDataSet(new Memo(cursor.getString(0), cursor.getString(1)));
         }
-        adapter.notifyDataSetChanged(); //데이터 변경 알림.
+        cursor.close();
     }
 
+    @Override
+    protected void onDestroy () {
+        super.onDestroy();
+        mDbHelper.close();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu ( Menu menu ) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected ( MenuItem item ) {
+        switch ( item.getItemId() ) {
+            case R.id.menu_write:
+                startActivity(new Intent(getApplicationContext(), WriteActivity.class));
+                return true;
+            default:
+                return true;
+        }
+    }
 }
 
--- app/src/main/java/me/jy/danggi/activities/adapter/MemoAdapter.java
+++ app/src/main/java/me/jy/danggi/activities/adapter/MemoAdapter.java
@@ -6,11 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
 import me.jy.danggi.R;
-import me.jy.danggi.databinding.ItemMemoBinding;
 import me.jy.danggi.activities.model.Memo;
+import me.jy.danggi.databinding.ItemMemoBinding;
 
 /**
  * Created by JY on 2018-01-12.
@@ -18,38 +21,74 @@ import me.jy.danggi.activities.model.Memo;
 
 public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.MemoViewHolder> {
 
-    private List<Memo> dataSet;
+    private List<Memo> dataSet = new ArrayList<>();
+    private PublishSubject<Integer> publishSubject;
 
-    public MemoAdapter(List<Memo> dataSet) {
-        this.dataSet = dataSet;
+    public MemoAdapter () {
+        this.publishSubject = PublishSubject.create();
     }
 
     @Override
-    public MemoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public MemoViewHolder onCreateViewHolder ( ViewGroup parent, int viewType ) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_memo, parent, false);
+
         return new MemoViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(MemoViewHolder holder, int position) {
+    public void onBindViewHolder ( MemoViewHolder holder, int position ) {
         Memo memo = dataSet.get(position);
-
         holder.binding.setMemo(memo);
-
+        holder.getLongClickObserver(position).subscribe(publishSubject);
     }
 
     @Override
-    public int getItemCount() {
+    public int getItemCount () {
         return (dataSet != null ? dataSet.size() : 0);
+    }
+
+    public void updateDataSet ( Memo data ) {
+        this.dataSet.add(data);
+        notifyDataSetChanged();
+    }
+
+    public void deleteDataSet ( int position ) {
+        dataSet.remove(position);
+        notifyDataSetChanged();
+    }
+
+    public void deleteDataSet (  ) {
+        dataSet.clear();
+        notifyDataSetChanged();
+    }
+
+
+    public PublishSubject<Integer> getPublishSubject () {
+        return this.publishSubject;
+    }
+
+    public Memo getDataFromPosition (int position) {
+        return dataSet.get(position);
     }
 
     static class MemoViewHolder extends RecyclerView.ViewHolder {
 
         ItemMemoBinding binding;
 
-        public MemoViewHolder(View itemView) {
+        private MemoViewHolder ( View itemView ) {
             super(itemView);
             binding = DataBindingUtil.bind(itemView);
         }
+
+        private Observable<Integer> getLongClickObserver ( int position ) {
+            return Observable.create(emitter -> {
+                itemView.setOnLongClickListener(v -> {
+                    emitter.onNext(position);
+                    return true;
+                });
+            });
+        }
+
+
     }
 }
