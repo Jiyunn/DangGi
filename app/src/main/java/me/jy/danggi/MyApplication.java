1bce328b10f4ac1dd9505991e324da1f2365034b
Jiyunn <jygr0309@gmail.com>
2018-04-04 14:46:06 +0900

    #54-Realm설정 및 gradle버전 업그레이드

--- app/src/main/java/me/jy/danggi/MyApplication.java
+++ app/src/main/java/me/jy/danggi/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 
+import io.realm.Realm;
+
 /** application class
  * Created by JY on 2018-01-19.
  */
@@ -12,6 +14,7 @@ public class MyApplication extends Application {
 
     public void onCreate() {
         super.onCreate();
+        Realm.init(this);
         Stetho.initializeWithDefaults(this);
     }
 }
