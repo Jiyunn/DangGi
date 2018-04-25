de220de4f948f300f1d9d97e8b2b6edbd0b04afd
Jiyunn <jygr0309@gmail.com>
2018-04-25 18:32:32 +0900

    #74-썸네일 저장 기능 수정

--- app/src/main/java/me/jy/danggi/binding/BindingConverts.java
+++ app/src/main/java/me/jy/danggi/binding/BindingConverts.java
@@ -1,10 +1,6 @@
 package me.jy.danggi.binding;
 
 import android.databinding.BindingConversion;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -21,12 +17,4 @@ public class BindingConverts{
     public static String convertDate( Date date ) {
         return new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm" , Locale.KOREAN).format(date);
     }
-
-    @BindingConversion
-    public static Drawable convertByteArrayToDrawable( byte[] bytes ) {
-        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes , 0 , bytes.length);
-        return new BitmapDrawable(bitmap);
-    }
-
-
 }
